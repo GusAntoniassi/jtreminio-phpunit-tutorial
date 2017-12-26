f70847d7cdbaa0b1a78235aa8e4cf723e06f0d09
GusAntoniassi <gus.antoniassi@gmail.com>
2017-12-26 18:59:19 -0200

    Parte 3 do tutorial, adicionados testes de cobertura

---
+++ tests/UserTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace App;
+
+use PHPUnit\Framework\TestCase;
+use App\User;
+
+class UserTest extends TestCase
+{
+
+    public function testSetPasswordReturnsTrueWhenPasswordSuccessfullySet()
+    {
+        $details = array();
+        $user = new User($details);
+
+        $password = 'fubar';
+        $result = $user->setPassword($password);
+
+        $this->assertTrue($result);
+    }
+
+    public function testGetUserReturnsUserWithExpectedValues()
+    {
+        $details = array();
+
+        $user = new User($details);
+
+        $password = 'fubar';
+
+        $user->setPassword($password);
+
+        $expectedPasswordResult = '5185e8b8fd8a71fc80545e144f91faf2';
+
+        $currentUser = $user->getUser();
+
+        $this->assertEquals($expectedPasswordResult, $currentUser['password']);
+    }
+}
